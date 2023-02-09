f93af70e5e41b63f302eff81576820a104f7bbe1
Abhi007-glitch <abhinav619000@gmail.com>
2023-02-10 01:54:59 +0530

    rectified

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 const About = ()=>{
     return (<div><h1>About us Page</h1>
-               <h2>Hello We are Namaste React Chef's</h2>
-        <h3>We provide a healthy and delecious webSites which increase your crave more</h3>
+               <h2>Hello We are FoodVilla Chef's</h2>
+        <h3>We provide a healthy and delecious Foods which increase your crave more</h3>
         </div>)
 }
 
