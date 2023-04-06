d6820475fccc6dae4feb447c7c4eb1c52428da29
Abhi007-glitch <abhinav619000@gmail.com>
2023-04-06 12:55:34 +0530

    resolving tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ "./src/**/*.{html,js}",],
+  purge: [
+    "./src/components/**/*.{js}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
